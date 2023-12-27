2172bd8f944b1c7444c6fa6dd167a5eb9b685059
Dika Bosnjak <dikabosnjak@Dikas-MacBook-Air.local>
2023-12-27 21:28:28 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace employee_management_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
